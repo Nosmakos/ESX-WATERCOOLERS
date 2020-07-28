881c8bad9dadf188b6b36a9d8443a862b286f97d
Nosmakos <32522858+Nosmakos@users.noreply.github.com>
2020-07-28 20:21:51 +0300

    Update client.lua

--- esx_tgo_watercoolers/client.lua
+++ esx_tgo_watercoolers/client.lua
@@ -25,7 +25,7 @@ Citizen.CreateThread(function()
                 if dist < 1.8 then
                     local loc = vector3(waterCoolerPos.x, waterCoolerPos.y, waterCoolerPos.z + 1.0)
                     
-                    ESX.Game.Utils.DrawText3D(loc, 'Press [~y~E~w~] to drink water.', 0.7)
+                    ESX.Game.Utils.DrawText3D(loc, 'Πατήστε [~y~E~w~] για να πείτε νερό.', 0.7)
                     if IsControlJustReleased(0, 38) then
 
                         if not IsAnimated then
@@ -54,6 +54,8 @@ Citizen.CreateThread(function()
                         end
                         
                     end
+                else
+                    Citizen.Wait(1500)
                 end
             end
         end
