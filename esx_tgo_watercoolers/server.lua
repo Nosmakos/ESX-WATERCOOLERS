501ad163767973bbeb9b7d98f525e6deb1596c44
Nosmakos <32522858+Nosmakos@users.noreply.github.com>
2020-07-31 15:59:18 +0300

    Update server.lua

--- esx_tgo_watercoolers/server.lua
+++ esx_tgo_watercoolers/server.lua
@@ -4,6 +4,5 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('esx_tgo_watercoolers:refillThirst')
 AddEventHandler('esx_tgo_watercoolers:refillThirst', function()
-	TriggerClientEvent('esx_status:add', source, 'thirst', 100000)
-	TriggerClientEvent('esx_status:add', source, 'hunger', 15000)
+	TriggerClientEvent('esx_status:add', source, 'thirst', 150000)
 end)
